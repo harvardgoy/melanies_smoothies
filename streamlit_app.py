0765479933ddd9c20dc1307bab7fe325f5a9cf15
harvardgoy <mhyasuda@outlook.com>
2024-05-26 12:51:42 -0600

    Update streamlit_app.py
    Connected changes needed to move from Sis to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -11,24 +11,13 @@ st.write(
     """Choose the fruits you want in your custom Smoothie!
     """)
 
-#option = st.selectbox(
-#    'How would you like to be contacted?',
-#    ('Email', 'Home phone', 'Mobile phone'))
-
-#st.write('You selected:', option)
-
-# option = st.selectbox(
-#    'What is your favorite fruit?',
-#    ('Banana', 'Strawberry', 'Peach'))
-
-# st.write('Your favorite fruit is:', option)
-
 #Lesson 4 - Add a name box for smoothie orders
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
